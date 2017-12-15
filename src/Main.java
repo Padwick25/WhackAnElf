23b65ef310e0e7e50b0074434391f8d4fc2d1e34
padwick25 <padwick25@gmail.com>
2017-12-15 12:16:10 +0000

    this is another commit that I am making, pls don't break

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		People mum = new People("paths\\Mum\\mumQ.txt");
+		People granddad = new People("paths\\granddad\\Q.txt");
 		Reader reader = new Reader();
+		granddad.argument(reader);
+		People mum = new People("paths\\mum\\Q.txt");
 		mum.argument(reader);
 	}
 	
