8f72f750e7320e19cf29bf307163601f30e9bd85
padwick25 <padwick25@gmail.com>
2017-12-15 13:00:43 +0000

    stuff

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Reader reader = new Reader();
 		
+		System.out.println("you are a QA consultant returning home for Christmas. \n you must run the gauntlet of familial conversation and interaction. \n Be careful how you respond your words could have dire consequences");
+		
 		People mum = new People("paths\\mum\\Q.txt");
 		mum.argument(reader);
 		
@@ -11,6 +13,8 @@ public class Main {
 		
 		People granddad = new People("paths\\granddad\\Q.txt");
 		granddad.argument(reader);
+		
+		System.out.println("");
 	}
 	
 	public static void printWhitespace() {
